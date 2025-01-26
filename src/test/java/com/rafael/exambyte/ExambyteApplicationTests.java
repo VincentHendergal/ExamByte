46eb1903ea63adbd25235f52f5aaf40823444bb5
VincentHendergal <rafaelsanchezsanchezsp@gmail.com>
2025-01-26 20:12:48 +0100

    Initial Commit

---
+++ src/test/java/com/rafael/exambyte/ExambyteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.exambyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExambyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
